8c577f9f1a20371909f574674394a76fd2b77c86
goatryder <nova9ok@gmail.com>
2021-06-24 23:46:39 +0300

    change texture color and translation using imgui

--- src/Application.cpp
+++ src/Application.cpp
@@ -56,11 +56,16 @@ int main(void)
 
     std::cout << glGetString(GL_VERSION) << std::endl;
     {
+        float textureSize[] = { 100.0f, 100.0f };
+        float textureMult[] = { 1.0f, 1.0f };
+        float textureOffset[] = { 100.0f, 100.0f };
+
+
         float positions[] = {
-             100.0f,  100.0f,  0.0f, 0.0f, // 0  first two are Position, next two are texCoord
-             200.0f,  100.0f,  1.0f, 0.0f, // 1
-             200.0f,  200.0f,  1.0f, 1.0f, // 2
-             100.0f,  200.0f,  0.0f, 1.0f  // 3
+             textureOffset[0],                                    textureOffset[1],                                    0.0f, 0.0f, // 0  first two are Position, next two are texCoord
+             textureOffset[0] + textureSize[0] * textureMult[0],  textureOffset[1],                                    1.0f, 0.0f, // 1
+             textureOffset[0] + textureSize[0] * textureMult[0],  textureOffset[1] + textureSize[1] * textureMult[1],  1.0f, 1.0f, // 2
+             textureOffset[0],                                    textureOffset[1] + textureSize[1] * textureMult[1],  0.0f, 1.0f  // 3
         };
 
         unsigned int indices[] = {
@@ -85,11 +90,10 @@ int main(void)
         // declare index buffer
         IndexBuffer ib(indices, 6);
 
-        // 4:3 screen format projection
+        // 16:9 screen format projection
         glm::mat4 proj = glm::ortho(0.0f, 960.0f, 0.0f, 540.0f, -1.0f, 1.0f);
         glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(-100.0f, 0.0f, 0.0f));
-        glm::mat4 model = glm::translate(glm::mat4(1.0f), glm::vec3(200.0f, 200.0f, 0.0f));
-        glm::mat4 mvp = proj * view * model;
+
 
         // to see in dbg mode what is going on with proj * float positions
         //glm::vec4 vp(100.0f, 100.0f, 0.0f, 1.0f);
@@ -98,8 +102,7 @@ int main(void)
         // create and run shader program on gpu
         Shader shader("res/shaders/Basic.shader");
         shader.Bind();
-        shader.SetUniform4f("u_Color", 0.2f, 0.3f, 0.8f, 1.0f);
-        shader.SetUniformMat4f("u_MVP", mvp);
+        shader.SetUniform4f("u_Color", 1.0f, 1.0f, 1.0f, 1.0f);
 
         Texture texture("res/textures/UE_Logo.png");
         texture.Bind();
@@ -133,14 +136,18 @@ int main(void)
         ImGui_ImplOpenGL3_Init("#version 130");
 
         // Our state
-        bool show_demo_window = false;
+        // bool show_demo_window = false;
         ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
         // //
 
         // color pulse
         float r = 0.0f;
         float increment = 0.05f;
-        //
+
+        // logo translation
+        glm::vec3 translation = glm::vec3(200.0f, 200.0f, 0.0f);
+        // logo color
+        ImVec4 color = { 1.0f, 1.0f, 1.0f, 1.0f };
 
         /* Loop until the user closes the window */
         while (!glfwWindowShouldClose(window))
@@ -165,19 +172,25 @@ int main(void)
 
             shader.Bind();
             //shader.SetUniform4f("u_Color", r, 0.3f, 0.5f, 1.0f);
+            shader.SetUniform4f("u_Color", color.x, color.y, color.z, color.w);
 
             // little focus to change color
-            if (r > 1.0f)
-                increment = -0.05f;
-            else if (r < 0.0f)
-                increment = 0.05f;
+            //if (r > 1.0f)
+            //    increment = -0.01f;
+            //else if (r < 0.0f)
+            //    increment = 0.01f;
 
-            r += increment;
+            //r += increment;
             //
 
+            // unreal logo texture translation
+            glm::mat4 model = glm::translate(glm::mat4(1.0f), translation);
+            glm::mat4 mvp = proj * view * model;
+            shader.SetUniformMat4f("u_MVP", mvp);
+
             // 1. Show the big demo window (Most of the sample code is in ImGui::ShowDemoWindow()! You can browse its code to learn more about Dear ImGui!).            
-            if (show_demo_window)
-                ImGui::ShowDemoWindow(&show_demo_window);
+            //if (show_demo_window)
+            //    ImGui::ShowDemoWindow(&show_demo_window);
 
             // 2. Show a simple window that we create ourselves. We use a Begin/End pair to created a named window.
             {
@@ -186,27 +199,28 @@ int main(void)
 
                 ImGui::Begin("Debug!");                          // Create a window called "Hello, world!" and append into it.
 
-                ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our window open/close state
+                //ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our window open/close state
 
-                ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
-                ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
-
-                // ImGui::SliderFloat3("Projection",);
+                //ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
+                
+                ImGui::ColorEdit3("Color", (float*)&color); // Edit 3 floats representing a color
+                ImGui::SliderFloat2("Translation", &translation.x, 0.f, 960.f);
+                ImGui::SliderFloat2("Size", (float*)(&textureMult), -5.0f, 5.0f);
 
                 ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
                 ImGui::End();
             }
 
-            /* Rendering */
-            renderer.Draw(va, ib, shader);
-
             ImGui::Render();
             int display_w, display_h;
             glfwGetFramebufferSize(window, &display_w, &display_h);
             glViewport(0, 0, display_w, display_h);
-            glClearColor(clear_color.x * clear_color.w, clear_color.y * clear_color.w, clear_color.z * clear_color.w, clear_color.w);
+            //glClearColor(clear_color.x * clear_color.w, clear_color.y * clear_color.w, clear_color.z * clear_color.w, clear_color.w);
             glClear(GL_COLOR_BUFFER_BIT);
             ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+            /* Rendering */
+            renderer.Draw(va, ib, shader);
 
             /* Swap front and back buffers */
             GLCall(glfwSwapBuffers(window));
